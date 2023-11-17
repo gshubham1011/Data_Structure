ea93d9529df5b6504f11d01d160228a784678ccc
shubham <gshubham1011@gmail.com>
2023-11-17 12:27:46 +0530

    linear search

---
+++ src/main/java/T_Searching_Algorithms/linear_Search.java
@@ -0,0 +1,21 @@
+package T_Searching_Algorithms;
+
+public class linear_Search {
+	 public static boolean linearSearch(int[] a, int searchKey) {
+		 
+		 for(int i=0;i<a.length;i++) {
+			 if(a[i]==searchKey) {
+				 return true;
+			 }
+		 }		 
+		 return false;
+	 }
+
+	public static void main(String[] args) {
+		int[] arr = {1,3,5,2,6,8};
+		System.err.println(" key found :"+linearSearch(arr,10));
+		
+
+	}
+
+}
